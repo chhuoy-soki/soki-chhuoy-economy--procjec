e46194fc5fdb1067fb364aff3f75c021cf65e9be
SOKI.CHHUOY <SOKI.CHHUOY@PNC.LAN>
2022-12-02 17:28:29 +0700

    [UPDATE BUTTON] buttno add product and edit

--- seller/js/script.js
+++ seller/js/script.js
@@ -1,17 +1,18 @@
 const table = document.querySelector("#table-container");
 const questions_dialog = document.querySelector("#questions-dialog")
 const create = document.querySelector("#create-product")
-const cacel = document.querySelector("#cancal-product")
+const header = document.querySelector("#edit-header")
+// const cacel = document.querySelector("#cancal-product")
 
 let products =[
-    {image:"../../image/dress-3.jpg",product:"Dress for girl free size",price: 45},
-    {image:"../../image/dress-2.jpg",product:"Dress for girl free size",price: 45},
-    {image:"../../image/dress-5.jpg",product:"Dress for girl free size",price: 45},
-    {image:"../../image/dress.jpg",product:"Dress for girl free size",price: 45},
-    {image:"../../image/dress4.jpg",product:"Dress for girl free size",price: 45},
-    {image:"../../image/T-shirt-2.jpg",product:"Dress for girl free size",price: 45},
-    {image:"../../image/T-shirt-4webp",product:"Dress for girl free size",price: 45},
-    {image:"../../image/dress.jpg",product:"Dress for girl free size",price: 45},
+    {image:"../../image/dress-3.jpg",name:"Dress for girl free size",price: 45},
+    {image:"../../image/dress-2.jpg",name:"Dress for girl free size",price: 45},
+    {image:"../../image/dress-5.jpg",name:"Dress for girl free size",price: 45},
+    {image:"../../image/dress.jpg",name:"Dress for girl free size",price: 45},
+    {image:"../../image/dress4.jpg",name:"Dress for girl free size",price: 45},
+    {image:"../../image/T-shirt-2.jpg",name:"Dress for girl free size",price: 45},
+    {image:"../../image/T-shirt-4webp",name:"Dress for girl free size",price: 45},
+    {image:"../../image/dress.jpg",name:"Dress for girl free size",price: 45},
 ]
 
 
@@ -23,25 +24,31 @@ function hide(element){
 }
 
 function onAddButton(){
+    create.textContent = "creat"
+    header.textContent = "create product"
+
+
+    document.querySelector("#choice_image").value = ""
+    document.querySelector("#choice_product").value = ""
+    document.querySelector("#choice_price").value = ""
+
     show(questions_dialog)
 }
 
-function savePorduct() {
+function StorageProduct() {
     //set data in localstorage
     localStorage.setItem("products",JSON.stringify(products))
 }
 
 function loadProduct(){
     let productsStorage = JSON.parse(localStorage.getItem("products"));
-    if (productsStorage.length > 0){
+    if (productsStorage !== null){
       products = productsStorage
     }
     else{
         localStorage.removeItem("products")
     }
 }
-
-
 
 function renderProduct(){
     loadProduct()
@@ -55,8 +62,10 @@ function renderProduct(){
     
     for (let index = 0; index < products.length; index++){ 
         let tr = document.createElement("tr");
+
         tr.setAttribute("id", "table-row")
         tr.dataset.index = index
+        console.log(tr)
         
         let td1 = document.createElement("td");
         td1.setAttribute("id","image");
@@ -67,7 +76,7 @@ function renderProduct(){
         
         let td2 = document.createElement("td");
         td2.setAttribute("id","product");
-        td2.textContent = products[index].product 
+        td2.textContent = products[index].name
         // console.log(td2)
         
         let td3 = document.createElement("td");
@@ -77,7 +86,7 @@ function renderProduct(){
         let td4 = document.createElement("td");
         td4.setAttribute("id","action")
         
-        // tr.dataset.index = index
+      
         
         let create = document.createElement("button");
         create.setAttribute("id","delete");
@@ -104,84 +113,77 @@ function renderProduct(){
     }
 }
 
+let productIndex = null;
 
-let index = 0
+function createProduct(event){
+    event.preventDefault()
+    // hide(questions_dialog)  //hide form input
+
+    if (document.querySelector("#choice_image").value != "" && document.querySelector("#choice_product").value !="" && document.querySelector("#choice_price").value != ""){
+
+        if (productIndex !== null){
+            let editProduct = products[productIndex];
+            editProduct.image = document.querySelector("#choice_image").value;
+            editProduct.name = document.querySelector("#choice_product").value;
+            editProduct.price = document.querySelector("#choice_price").value;
+            // create new product
+            // products = null
+        }
+        else{
+            let newProduct = {}
+            newProduct.image = document.querySelector("#choice_image").value;
+            newProduct.name = document.querySelector("#choice_product").value;
+            newProduct.price = document.querySelector("#choice_price").value;
+            products.push(newProduct) 
+        }
+        hide(questions_dialog)
+        StorageProduct()
+        renderProduct()
+    }
+    else{
+        alert("You must input a product")
+    }
+}
+
+
 function onEdit(event){
     //edit information in product
 
     show(questions_dialog)
-    document.querySelector("menu").lastElementChild.textContent = "Edit"
-
-    index = event.target.parentElement.parentElement.parentElement.dataset.index;
     
-    document.querySelector("#choice_image").value = products[index].image;
-    document.querySelector("#choice_product").value = products[index].product;
-    document.querySelector("#choice_price").value = products[index].price;
-    
+
+    let newProduct = event.target.parentElement.parentElement.parentElement.dataset.index;
+    productIndex = newProduct
+    let editProduct = products[newProduct];
+    document.querySelector("#choice_image").value = editProduct.image;
+    document.querySelector("#choice_product").value = editProduct.name;
+    document.querySelector("#choice_price").value = editProduct.price;
+    create.textContent = "save"
+    header.textContent = "edit product"
+
+    console.log(create)
+
 }
-
-function add(){
-    clearData()
-    show(questions_dialog)
-    index = null
-}
-
-function editProduct(){
-
-    hide(questions_dialog)
-
-    if (products !== null){  /// if don't have value
-        document.querySelector("#choice_image").value = products[index].image;
-        document.querySelector("#choice_product").value = products[index].product;
-        document.querySelector("#choice_price").value = products[index].price;
-    }else{
-        //if have value
-        let newProduct = {}
-        newProduct.image = document.querySelector("#choice_image").value;
-        newProduct.product = document.querySelector("#choice_product").value;
-        newProduct.price = document.querySelector("#choice_price").value;
-        products.push(newProduct)
-    }
-}
-
-function createProduct(){
-    hide(questions_dialog)  //hide form input
-
-    // create new product
-    let newProduct = {}
-    newProduct.image = document.querySelector("#choice_image").value;
-    newProduct.product = document.querySelector("#choice_product").value;
-    newProduct.price = document.querySelector("#choice_price").value;
-    products.push(newProduct)
-    savePorduct()
-    renderProduct()
-}
-
-
 
 function onDelete(event){
     // remove product
+    event.preventDefault();
 
     let index = event.target.parentElement.parentElement.parentElement.dataset.index;
     console.log(products)
     products.splice(index, 1);
-    savePorduct()
+    StorageProduct()
     renderProduct()
 
 }
 
-function clearData(){
-    //clear value in input
-    document.querySelector("#choice_image").value = ""
-    document.querySelector("#choice_product").value = ""
-    document.querySelector("#choice_price").value = ""
-}
 
-function onCancel(){
+function onCancel(event){
+    event.preventDefault()
     hide(questions_dialog)
 }
 
 renderProduct()
 loadProduct()
-savePorduct()
+// StorageProduct()
 
